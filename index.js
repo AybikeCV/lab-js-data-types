51ae2c1fb097e4b5793f4449a9043bade5a0cf0d
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-04-01 17:42:02 +0200

    Solved lab

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `"${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}"`
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -23,11 +24,13 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const parT1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const parT2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+const combineD = `"${parT1}${parT2}"`
 
 // Print the cameLtaiL-formatted string
 
-
-
+console.log(combineD)
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
@@ -36,9 +39,10 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal *.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -48,10 +52,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNumber = Math.floor(Math.random() * 10) + 1
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +66,18 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+
+console.log(expression1, expression2, expression3, expression4, expression5, expression6, expression7)
